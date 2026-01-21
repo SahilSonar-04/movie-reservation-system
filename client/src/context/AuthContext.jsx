13811aeb8f8878caecaa0550a5e3f7ed7c44921b
SahilSonar-04 <sonarsahil4@gmail.com>
2026-01-21 17:15:05 +0530

    fixed frontend if

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -34,7 +34,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const res = await api.post("/auth/login", { email, password });
+      const res = await api.post("/api/auth/login", { email, password });
       localStorage.setItem("token", res.data.token);
 
       const decoded = jwtDecode(res.data.token);
